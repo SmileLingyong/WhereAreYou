c76f08814d499eab01ee9bbb1e22b8a432a3b30e
smile <smilelingyong@163.com>
2017-10-12 23:40:51 +0800

    add Entry Activity

---
+++ app/src/main/java/com/example/smile/whereareyou/activity/EntryActivity.java
@@ -0,0 +1,101 @@
+package com.example.smile.whereareyou.activity;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.app.Activity;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+import com.example.smile.whereareyou.R;
+
+import java.util.Random;
+
+
+/**
+ * Created by lly54 on 2017/10/11.
+ */
+
+public class EntryActivity extends Activity {
+    private static final int ANIMATION_DURATION = 2000;
+    private static final float SCALE_END = 1.13F;
+
+    private static final int[] SPLASH_ARRAY = {
+            R.drawable.splash0,
+            R.drawable.splash2,
+            R.drawable.splash4,
+            R.drawable.splash5,
+            R.drawable.splash6,
+            R.drawable.splash7,
+            R.drawable.splash8,
+            R.drawable.splash9,
+            R.drawable.splash10,
+            R.drawable.splash11,
+            R.drawable.splash15,
+            R.drawable.splash16,
+    };
+
+    ImageView mSplashImage;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initWindow();
+        setContentView(R.layout.activity_entry);
+
+        mSplashImage = (ImageView) findViewById(R.id.iv_enter);
+
+        //通过Random生成随机数，然后通过这个随机数转换成int类型调用数组中的元素，即一张张图片
+        Random r = new Random(SystemClock.elapsedRealtime());
+        mSplashImage.setImageResource(SPLASH_ARRAY[r.nextInt(SPLASH_ARRAY.length)]);
+        animateImage();
+    }
+
+    private void initWindow(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT){
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        }
+    }
+
+    //设置启动界面的动画效果
+    private void animateImage(){
+        ObjectAnimator animatorX = ObjectAnimator.ofFloat(mSplashImage, "scaleX", 1f, SCALE_END);
+        ObjectAnimator animatorY = ObjectAnimator.ofFloat(mSplashImage, "scaleY", 1f, SCALE_END);
+
+        AnimatorSet set = new AnimatorSet();
+        //设置动画的长度：通过上面定义的animatorX，animatorY来渐变长度
+        set.setDuration(ANIMATION_DURATION).play(animatorX).with(animatorY);
+        set.start();
+
+        set.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                MainActivity.start(EntryActivity.this);
+                EntryActivity.this.finish();
+            }
+        });
+
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+
+}
